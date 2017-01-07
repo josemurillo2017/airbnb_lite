d8eff6b3f4a59d28fba30d510f523aa117c71513
Raghu Betina <raghu@firstdraft.com>
2017-01-07 01:32:39 +0000

    Whitelist columns in Policy for ActiveAdmin

--- app/admin/policy.rb
+++ app/admin/policy.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Policy do
 
+ permit_params :description
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
