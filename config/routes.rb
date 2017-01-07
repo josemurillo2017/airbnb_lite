eae4fc4cdc014ff62f97174b4982cf9adb346709
Raghu Betina <raghu@firstdraft.com>
2017-01-07 01:31:44 +0000

    Generate guests resource with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :guests
+  # Routes for the Guest resource:
+  # READ
+  get "/guests", :controller => "guests", :action => "index"
+  get "/guests/:id", :controller => "guests", :action => "show"
+
+
   # Routes for the Availibiliity resource:
   # CREATE
   get "/availibiliities/new", :controller => "availibiliities", :action => "new"
